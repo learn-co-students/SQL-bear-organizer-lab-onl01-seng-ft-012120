1dca164b5bc7cbd8d42dc3874efbd202f154ac28
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-02-19 22:28:13 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,23 @@
-INSERT INTO cats(Mr. Chocolate,
-Rowdy ,
-Tabitha ,
-Sergeant Brown,
-Melissa ,
-Grinch ,
-Wendy ,
+INSERT INTO bears (name,age,gender,color, temperament, alive) VALUES ("Mr. Chocolate",12,"male","blue","2 degree",1);
 
-unname null) VALUES(1,2,3,4,5,6,null);
+INSERT INTO bears (name,age,gender,color, temperament, alive) VALUES ("Mr. Chocolate1",121,"male1","blue1","21 degree",11);
+
+INSERT INTO bears (name,age,gender,color, temperament, alive) VALUES ("Mr. Chocolate11",1211,"male11","blue11","211 degree",111);
+INSERT INTO bears (name,age,gender,color, temperament, alive) VALUES ("Mr. Chocolate12",1212,"male12","blue12","212 degree",112);
+
+INSERT INTO bears (name,age,gender,color, temperament, alive) VALUES ("Mr. Chocolate14",1214,"male14","blue14","214 degree",114);
+
+INSERT INTO bears (name,age,gender,color, temperament, alive) VALUES ("Mr. Chocolate1",1215,"male15","blue15","215 degree",115);
+
+INSERT INTO bears (name,age,gender,color, temperament, alive) VALUES ("Mr. Chocolate1y",1216,"male1","blue16","216 degree",116);
+
+INSERT INTO bears (name,age,gender,color, temperament, alive) VALUES (null,1217,"male17","blue17","21 7degree",117);
+
+
+
+
+
+
+
+
+
