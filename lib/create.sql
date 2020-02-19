0a0868866b349ec5149c0d6d6412804e9372b448
Alehandro Persaud <andrew041994@gmail.com>
2020-02-19 00:43:36 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+
+    CREATE TABLE bears (
+      name TEXT,
+      age INTEGER,
+      gender TEXT,
+      color TEXT,
+      temperament TEXT,
+      alive boolean,
+      id INTEGER primary key
+    );
