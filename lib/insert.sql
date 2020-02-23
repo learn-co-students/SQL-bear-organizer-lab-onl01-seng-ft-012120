6d2f3e9e25ec4d2caf84d9eb83619bde405c762a
Agnes Park <agnespark824@gmail.com>
2020-02-23 22:46:44 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 44, "male", "brown", "carefree", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "male", "brown", "rambunctious", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "female", "polar", "placid", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 56, "male", "black", "sociable", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 44, "male", "black", "pessimistic", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 112, "male", "brown", "angry", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 16, "female", "polar", "sleepy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 65, "female", "brown", "carefree", 0);
